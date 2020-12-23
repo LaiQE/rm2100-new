4cd900bccfdb685996e1e4d1448b2896bfe6f365
LaiQE <laiqe@zju.edu.cn>
2020-12-23 22:14:16 +0800

    Update diy-part2.sh
    增加了一些内容,和suling的保持一致

--- diy-part2.sh
+++ diy-part2.sh
@@ -102,3 +102,37 @@ git clone --depth=1 https://github.com/project-openwrt/openwrt-tmate
 git clone --depth=1 https://github.com/tindy2013/openwrt-subconverter
 
 popd
+# Mod zzz-default-settings
+pushd package/lean/default-settings/files
+sed -i "/commit luci/i\uci set luci.main.mediaurlbase='/luci-static/argon'" zzz-default-settings
+sed -i '/http/d' zzz-default-settings
+sed -i '/exit/i\chmod +x /bin/ipv6-helper' zzz-default-settings
+popd
+
+# Mod ipv6-helper.sh
+sed -i '/filter_aaaa/d;/commit dhcp/d' ../scripts/ipv6-helper.sh
+
+# Remove orig kcptun
+rm -rf ./feeds/packages/net/kcptun
+
+# Max connections
+sed -i 's/16384/65536/g' package/kernel/linux/files/sysctl-nf-conntrack.conf
+
+# Remove IPV6
+sed -i 's/ip6tables //g' include/target.mk
+sed -i 's/odhcpd-ipv6only odhcp6c //g' include/target.mk
+
+# Change dnsmasq to dnsmasq-full
+sed -i 's/dnsmasq i/dnsmasq-full i/g' include/target.mk
+
+# Add po2lmo
+git clone https://github.com/openwrt-dev/po2lmo.git
+pushd po2lmo
+make && sudo make install
+popd
+
+# Fix mt76 wireless driver
+pushd package/kernel/mt76
+sed -i '/mt7662u_rom_patch.bin/a\\techo mt76-usb disable_usb_sg=1 > $\(1\)\/etc\/modules.d\/mt76-usb' Makefile
+popd
+
